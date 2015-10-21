c9b481178d94198f931827817668670fc0c2368a
Viliam Simko <viliam.simko@gmail.com>
2015-10-21 23:37:56 +0200

    Documented datasets

--- R/package.R
+++ R/package.R
@@ -2,3 +2,29 @@
 #' @import reshape2
 #' @import scales
 NULL
+
+#' Simple traffic example of 32 samples for debugging
+"sample32"
+
+
+#' Example of heat-up phase with large data transfer
+#' 
+#' Irregular data transfer with a heat-up phase containing several small data
+#' transfers, followed by a continuous transfer reaching the link capacity.
+"sample_linkcap"
+
+
+#' Example of regular signal
+"sample_regsignal"
+
+
+#' Video streaming traffic
+#' 
+#' Data transfer during 60 minutes of video streaming with occasional caching.
+"sample_vidstream"
+
+
+#' Signal with several outliers.
+#' 
+#' An example of multiple applications transferring data in parallel.
+"sample_withoutliers"
